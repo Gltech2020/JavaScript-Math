8fea5a292d2d5a50ae916f319d7b2172ed5e42b1
Gladys <77966043+Gltech2020@users.noreply.github.com>
2024-04-23 14:21:10 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let price = 4.5;
+
+price = Math.round(price);
+console.log(price);
+let price2 = 4.5;
+price2 = Math.ceil(price2);
+console.log(price2);
+let price3 = 4.5;
+price3 = Math.floor(price3);
+console.log(price3);
+console.log(Math.min(4, 1, 3, 10));
+console.log(Math.max(4, 1, 3, 10));
